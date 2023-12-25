f23c5bbe79a90ac31e788d2899b52a85cfc35d78
fabio-lucs <fabiolucs19@gmail.com>
2023-12-25 18:36:26 -0300

    up

--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+class Program
+{
+    static void Main()
+    {
+
+        Iphone iphone = new Iphone("123456789", "iPhone X", "IMEI123", 64);
+        Nokia nokia = new Nokia("987654321", "Nokia 3310", "IMEI456", 16);
+
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine(); 
+
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Snake");
+
+        Console.ReadLine(); 
+
+    }
+}
